a9dafdaa37f8a2df88454a27e232234750573f15
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-04-21 02:09:06 +0300

    3 lect

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0319;
+
+/* 
+Предсказание на будущее
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0320/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0320;
+
+/* 
+Скромность украшает программиста
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
