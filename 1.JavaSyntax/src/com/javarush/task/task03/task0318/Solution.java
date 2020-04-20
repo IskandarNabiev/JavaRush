14d048bc8cc8be0244cc2813a8c3b905a0ddf5f5
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-04-20 23:10:21 +0300

    3 lect

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task03.task0318;
+
+/* 
+План по захвату мира
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
