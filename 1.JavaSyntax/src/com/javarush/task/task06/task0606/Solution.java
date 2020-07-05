6619fb61bc9eee634b26f10069bbfd61dfaae980
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-07-05 15:54:20 +0300

    6th level

--- 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -7,7 +7,6 @@ package com.javarush.task.task06.task0606;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Solution {
 
@@ -15,6 +14,17 @@ public class Solution {
     public static int odd;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(reader.readLine());
+        while (num != 0) {
+            if (num % 2 == 0) {
+                even++;
+            }
+            else odd++;
+            num = num / 10;
+
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
 
     }
 }
