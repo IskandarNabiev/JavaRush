2291020920526e7df62cf59a49c5bfb17924661f
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-07-25 12:50:57 +0300

    3nd level, 11th lecture Java Core

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -4,8 +4,25 @@ package com.javarush.task.task13.task1319;
 Писатель в файл с консоли
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        File myFile = new File(reader.readLine());
+        BufferedWriter filewrite = new BufferedWriter(new FileWriter(myFile));
+        try {
+            while (true) {
+                String text = reader.readLine();
+                filewrite.write(text + System.lineSeparator());
+                if (text.equals("exit")) {
+                    break;
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        filewrite.close();
     }
 }
