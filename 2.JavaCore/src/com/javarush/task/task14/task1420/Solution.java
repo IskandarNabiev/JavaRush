95b8e7281f024dbb12d0461dbd8aef9142e8bf4f
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-07-28 00:16:03 +0300

    4th level, 8th lecture Java Core

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task14.task1420;
+
+/* 
+НОД
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+        if ((num1 > 0) && (num2 > 0)) {
+            System.out.println(nod(num1, num2));
+        } else {
+            throw new Exception();
+        }
+
+    }
+    public static int nod (int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
