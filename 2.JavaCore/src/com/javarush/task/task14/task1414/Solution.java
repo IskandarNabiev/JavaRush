5f59911a3b3f7bd3e2031ddac7adefeaf7f364e8
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-08-02 00:23:38 +0300

    Java Core 4th level, 8th lecture

--- 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -5,40 +5,40 @@ MovieFactory
 */
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //ввести с консоли несколько ключей (строк), пункт 7
-
-        /*
-8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
-8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
-8.2 вывести на экран movie.getClass().getSimpleName()
-        */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while(true)
+        {
+            String key = reader.readLine();
+            Movie movie = MovieFactory.getMovie(key);
+            if(null == movie)
+            {
+                break;
+            }
+            System.out.println(movie.getClass().getSimpleName());
+        }
 
     }
 
     static class MovieFactory {
 
-        static Movie getMovie(String key) throws IOException {
+        static Movie getMovie(String key) {
             Movie movie = null;
-            Cartoon cartoon = null;
-            Thriller thriller = null;
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String str = reader.readLine();
-
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
             if ("soapOpera".equals(key)) {
                 movie = new SoapOpera();
             }
             else if ("cartoon".equals(key)) {
-                cartoon = new Cartoon();
+                movie = new Cartoon();
             }
             else if ("thriller".equals(key)) {
-                thriller = new Thriller();
+                movie = new Thriller();
             }
 
             return movie;
@@ -51,12 +51,11 @@ public class Solution {
     static class SoapOpera extends Movie {
     }
 
-    public static class Cartoon {
+    static class Cartoon extends Movie {
 
     }
 
-
-    public static class Thriller {
+    static class Thriller extends Movie{
 
     }
 }
