4fea8bb076a670faa3614625f3cecdd6fa2c2ac7
IskandarNabiev <iskandarnabiev96@gmail.com>
2020-08-23 23:10:43 +0300

    Java Core 6th level, 13th lecture

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1626/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task16.task1626;
+
+public class Solution {
+    public static int number = 5;
+
+    public static void main(String[] args) {
+        new Thread(new CountdownRunnable(), "Уменьшаем").start();
+        new Thread(new CountUpRunnable(), "Увеличиваем").start();
+    }
+
+    public static class CountUpRunnable implements Runnable {
+        private int countIndexUp = Solution.number;
+        public void run() {
+            try {
+
+                for (int i = 1; i <= countIndexUp; i++) {
+                    Thread.sleep(500);
+                    System.out.println(Thread.currentThread().getName() + ": " + i);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static class CountdownRunnable implements Runnable {
+        private int countIndexDown = Solution.number;
+
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(toString());
+                    countIndexDown -= 1;
+                    if (countIndexDown == 0) return;
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexDown;
+        }
+    }
+}
